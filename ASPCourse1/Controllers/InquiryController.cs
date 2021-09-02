eb08ae621564fec6404bd73c158f6af33cc0966e
Michel Leblond <Michel.Leblond@parker.com>
2021-09-02 11:41:55 -0700

    last section of 4

---
+++ ASPCourse1/Controllers/InquiryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart_DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+        
+        public InquiryController(IInquiryHeaderRepository inqHRepo,IInquiryDetailRepository inqDRepo)
+        {
+            _inqDRepo = inqDRepo;
+            _inqHRepo = inqHRepo;
+
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
